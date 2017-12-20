a8d7a04830ead8c2136f21c3c32a2f76728bf6c7
Matheus Almeida <mat.almeida@live.com>
2017-12-20 00:04:18 -0300

    first commit

---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+// import classnames from 'classnames';
+
+import './style.css';
+
+export default class About extends Component {
+  render() {
+    // const { className, ...props } = this.props;
+
+    return (
+      <div className='About'>
+        <h1>
+          About
+        </h1>
+      </div>
+    )
+  }
+}
